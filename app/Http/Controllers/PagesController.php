bdd8aff3729bcbd7ef85012067d6960d40b3736c
朱智森 <596885319@qq.com>
2018-10-30 17:45:26 +0800

    邮箱验证中间件

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,4 +9,9 @@ class PagesController extends Controller
     public function root(){
         return view('pages.root');
     }
+
+    public function emailVerifyNotice(Request $request)
+    {
+        return view('pages.email_verify_notice');
+    }
 }
