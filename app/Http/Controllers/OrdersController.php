95ca4eb3cf2e7ea0ddfc2f1abb18e8d9d6f08a30
zzs <596885319@qq.com>
2018-11-07 18:03:25 +0800

    用户界面 - 订单列表页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,9 +8,23 @@ use App\Models\ProductSku;
 use App\Models\UserAddress;
 use App\Models\Order;
 use Carbon\Carbon;
+use App\Http\Requests\Request;
 
 class OrdersController extends Controller
 {
+
+
+    public function index(Request $request){
+        $orders = Order::query()
+            ->with(['items.product','items.productSku'])
+            ->where('user_id',$request->user()->id)
+            ->orderBy('created_at','desc')
+            ->paginate();
+
+        return view('orders.index', ['orders' => $orders]);
+    }
+
+
     public function store(OrderRequest $request)
     {
         $user  = $request->user();
