122cd7c7268f212ec56c8eae804e9eb01c5ecb13
zzs <596885319@qq.com>
2018-11-07 18:14:47 +0800

    用户界面 - 订单详情页

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -24,6 +24,11 @@ class OrdersController extends Controller
         return view('orders.index', ['orders' => $orders]);
     }
 
+    public function show(Order $order,Request $request){
+        $this->authorize('own', $order);
+        return view('orders.show',['order'=>$order->load(['items.productSku', 'items.product'])]);
+    }
+
 
     public function store(OrderRequest $request)
     {
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -16,6 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         UserAddress::class => UserAddressPolicy::class,
+        Order::class       => OrderPolicy::class,
     ];
 
     /**
