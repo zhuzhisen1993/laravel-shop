cd04c17f50c820614d3135afbdec1b47bc5c8cba
zzs <596885319@qq.com>
2018-11-08 12:01:54 +0800

    封装业务代码

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use App\Policies\UserAddressPolicy;
 use App\Models\UserAddress;
+use App\Models\Order;
+use App\Policies\OrderPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
